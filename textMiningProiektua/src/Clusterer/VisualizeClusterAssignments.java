a4b7770655ff4a14c3c8c8c7d699e8e64c93c570
JonBarbero <jonbarbero5@gmail.com>
2021-04-16 19:13:39 +0200

    Visualize gehituta

--- textMiningProiektua/src/Clusterer/VisualizeClusterAssignments.java
+++ textMiningProiektua/src/Clusterer/VisualizeClusterAssignments.java
@@ -1,80 +1,39 @@
 package Clusterer;
 
-import weka.clusterers.*;
-import weka.core.*;
-import weka.core.converters.ConverterUtils.*;
-import weka.gui.explorer.ClustererPanel;
-import weka.gui.visualize.*;
+import java.awt.Component;
 
-import java.awt.*;
-import java.io.*;
-import java.text.*;
-import java.util.*;
+import javax.swing.JFrame;
 
-import javax.swing.*;
+import weka.core.Instances;
+import weka.gui.visualize.PlotData2D;
 
-/**
- * Runs a clusterer on a dataset and visualizes the cluster assignments, 
- * like with right-click menu in Explorer.
- * <p/>
- * Takes two arguments:
- * <ol>
- *   <li>-t dataset</li>
- *   <li>-W cluster algorithm with options</li>
- * </ol>
- *
- * Note: code should work with Weka 3.6.0 and 3.5.8.
- *
- * @author FracPete (fracpete at waikato dot ac dot nz)
- */
 public class VisualizeClusterAssignments {
-  public static void main(String[] args) throws Exception {
-    // load data
-    Instances train = DataSource.read(Utils.getOption('t', args));
-    // some data formats store the class attribute information as well
-    if (train.classIndex() != -1)
-      throw new IllegalArgumentException("Data cannot have class attribute!");
+	
+		public static void main(String args[]) throws Exception {
+		     // train classifier
+			/*J48 cls = new J48();
+		     Instances data = new Instances(new BufferedReader(new FileReader("/home/jon/Descargas/heart-c.arff")));
+		     data.setClassIndex(data.numAttributes() - 1);
+		     cls.buildClassifier(data);
 
-    // instantiate clusterer
-    String[] options = Utils.splitOptions(Utils.getOption('W', args));
-    String classname = options[0];
-    options[0] = "";
-    Clusterer clusterer = AbstractClusterer.forName(classname, options);
-    
-    // evaluate clusterer
-    clusterer.buildClusterer(train);
-    ClusterEvaluation eval = new ClusterEvaluation();
-    eval.setClusterer(clusterer);
-    eval.evaluateClusterer(train);
+		     // display classifier
+		     final javax.swing.JFrame jf = 
+		       new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+		     jf.setSize(1250,1000);
+		     jf.getContentPane().setLayout(new BorderLayout());
+		     TreeVisualizer tv = new TreeVisualizer(null,
+		         cls.graph(),
+		         new PlaceNode2());
+		     jf.getContentPane().add(tv, BorderLayout.CENTER);
+		     jf.addWindowListener(new java.awt.event.WindowAdapter() {
+		       public void windowClosing(java.awt.event.WindowEvent e) {
+		         jf.dispose();
+		       }
+		     });
 
-    // setup visualization
-    // taken from: ClustererPanel.startClusterer()
-    PlotData2D predData = ClustererPanel.setUpVisualizableInstances(train, eval);	//no rula
-    String name = (new SimpleDateFormat("HH:mm:ss - ")).format(new Date());
-    String cname = clusterer.getClass().getName();
-    if (cname.startsWith("weka.clusterers."))
-      name += cname.substring("weka.clusterers.".length());
-    else
-      name += cname;
+		     jf.setVisible(true);
+		     tv.fitToScreen();
+		   }*/
 
-    VisualizePanel vp = new VisualizePanel();
-    vp.setName(name + " (" + train.relationName() + ")");
-    predData.setPlotName(name + " (" + train.relationName() + ")");
-    vp.addPlot(predData);
-
-    // display data
-    // taken from: ClustererPanel.visualizeClusterAssignments(VisualizePanel)
-    String plotName = vp.getName();
-    final javax.swing.JFrame jf = 
-      new javax.swing.JFrame("Weka Clusterer Visualize: " + plotName);
-    jf.setSize(500,400);
-    jf.getContentPane().setLayout(new BorderLayout());
-    jf.getContentPane().add(vp, BorderLayout.CENTER);
-    jf.addWindowListener(new java.awt.event.WindowAdapter() {
-      public void windowClosing(java.awt.event.WindowEvent e) {
-        jf.dispose();
-      }
-    });
-    jf.setVisible(true);
-  }
+		}
 }
