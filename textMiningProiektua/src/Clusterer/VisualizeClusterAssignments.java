1a92b3a06cd50e441143022ab08afaad37cb1f46
JonBarbero <jonbarbero5@gmail.com>
2021-04-17 16:45:24 +0200

    ThresholdCurve gehituta

--- textMiningProiektua/src/Clusterer/VisualizeClusterAssignments.java
+++ textMiningProiektua/src/Clusterer/VisualizeClusterAssignments.java
@@ -1,31 +1,57 @@
 package Clusterer;
 
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.evaluation.ThresholdCurve;
+import weka.classifiers.functions.SMO;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.gui.visualize.PlotData2D;
+import weka.gui.visualize.ThresholdVisualizePanel;
+
 public class VisualizeClusterAssignments {
+	
 		public static void main(String args[]) throws Exception {
-		     // train classifier
-			/*J48 cls = new J48();
-		     Instances data = new Instances(new BufferedReader(new FileReader("/home/jon/Descargas/heart-c.arff")));
-		     data.setClassIndex(data.numAttributes() - 1);
-		     cls.buildClassifier(data);
+			
+			DataSource source = new DataSource(args[0]);
+			Instances data= source.getDataSet();
+			data.setClassIndex(data.numAttributes()-1);
+			
+			Classifier smo = new SMO();
+			Evaluation eval = new Evaluation(data);
+			eval.crossValidateModel(smo, data, 10, new Random(1));
+			
+			ThresholdCurve tc = new ThresholdCurve();
+			int classIndex = 0;
+			Instances result = tc.getCurve(eval.predictions(), classIndex);
+			
+			// plot curve
+			ThresholdVisualizePanel vmc = new ThresholdVisualizePanel();
+			vmc.setROCString("(Area under ROC = " + ThresholdCurve.getROCArea(result) + ")");
+			vmc.setName(result.relationName());
+			PlotData2D tempd = new PlotData2D(result);
+			tempd.setPlotName(result.relationName());
+			tempd.addInstanceNumberAttribute();
+			
+			// specify which points are connected
+			boolean[] cp = new boolean[result.numInstances()];
+			for (int n = 1; n < cp.length; n++)
+				cp[n] = true;
+			tempd.setConnectPoints(cp);
 
-		     // display classifier
-		     final javax.swing.JFrame jf = 
-		       new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
-		     jf.setSize(1250,1000);
-		     jf.getContentPane().setLayout(new BorderLayout());
-		     TreeVisualizer tv = new TreeVisualizer(null,
-		         cls.graph(),
-		         new PlaceNode2());
-		     jf.getContentPane().add(tv, BorderLayout.CENTER);
-		     jf.addWindowListener(new java.awt.event.WindowAdapter() {
-		       public void windowClosing(java.awt.event.WindowEvent e) {
-		         jf.dispose();
-		       }
-		     });
-
-		     jf.setVisible(true);
-		     tv.fitToScreen();
-		   }*/
-
-		}
+			// add plot
+			vmc.addPlot(tempd);
+			
+			// display curve
+			JFrame frameRoc = new javax.swing.JFrame("ROC Curve");
+			frameRoc.setSize(1000, 700);
+			frameRoc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frameRoc.getContentPane().add(vmc);
+			frameRoc.setVisible(true);
+			}
 }
+
